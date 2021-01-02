5d776e925f7c85d0adcaf8f72690a558722ca550
Noe Guisset <noe.guisset@gmail.com>
2021-01-02 19:54:52 +0100

    added functions about compliance+tests

--- internal/clients/team3/clientgeneral_test.go
+++ internal/clients/team3/clientgeneral_test.go
@@ -3,6 +3,7 @@ package team3
 // General client functions testing
 
 import (
+	"math"
 	"reflect"
 	"testing"
 
@@ -227,6 +228,62 @@ func TestUpdateCriticalThreshold(t *testing.T) {
 			ans := tc.ourClient.criticalStatePrediction
 			if ans != tc.expected {
 				t.Errorf("got %f-%f, want %f-%f", ans.lowerBound, ans.upperBound, tc.expected.lowerBound, tc.expected.upperBound)
+			}
+		})
+	}
+}
+
+func TestUpdateCompliance(t *testing.T) {
+	cases := []struct {
+		name        string
+		ourClient   client
+		expectedVal float64
+	}{
+		{
+			name: "Just caught!",
+			ourClient: client{
+				timeSinceCaught: 0,
+				numTimeCaught:   100,
+				compliance:      0.2,
+				params: islandParams{
+					recidivism:      1.0,
+					complianceLevel: 0.1,
+				},
+			},
+			expectedVal: 1.0,
+		},
+		{
+			name: "Compliance decay - non-compliant agent",
+			ourClient: client{
+				timeSinceCaught: 1,
+				numTimeCaught:   1,
+				compliance:      1.0,
+				params: islandParams{
+					recidivism:      1.0,
+					complianceLevel: 0.0,
+				},
+			},
+			expectedVal: math.Exp(-0.5),
+		},
+		{
+			name: "Compliance decay - fully-compliant agent",
+			ourClient: client{
+				timeSinceCaught: 1,
+				numTimeCaught:   10,
+				compliance:      1.0,
+				params: islandParams{
+					recidivism:      1.0,
+					complianceLevel: 1.0,
+				},
+			},
+			expectedVal: 1.0,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.ourClient.updateCompliance()
+			if tc.ourClient.compliance != tc.expectedVal {
+				t.Errorf("Expected final transgressions to be %v got %v", tc.expectedVal, tc.ourClient.compliance)
 			}
 		})
 	}
